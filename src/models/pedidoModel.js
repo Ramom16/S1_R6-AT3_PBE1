20c168c28753bb85e89c85c3cc7b1884309cd1ca
unknown <sabrinaramom09@gmail.com>
2025-11-17 15:38:16 -0300

    logistica2

--- src/models/pedidoModel.js
+++ src/models/pedidoModel.js
@@ -6,38 +6,13 @@ const pedidoModel = {
             const connection = await pool.getConnection();
             await connection.beginTransaction();
             //insert 1 - tabela pedidos 
-            const sqlPedido = 'INSERT INTO pedidos (id_cliente, valor_total, data_pedido) VALUES (?,?,?);'
+            const sqlPedido = 'INSERT INTO pedidos (idCliente_fk, dataPedido, tipoEntrega, distanciaKm, pesoKg, valorBaseKm, valorBaseKg) VALUES (?,?,?,?,?,?,?);'
             const valuesPedido = [pIdCliente, pQuantidadeItem * pValorItem, pDataPedido];
             const [rowsPedido] = await connection.query(sqlPedido, valuesPedido);
-
-            // insert 2 = tabela itens_pedido
-
-            const sqlItem = 'INSERT INTO itens_pedidos (id_pedido_fk, id_produto_fk, quantidade, valor_item) VALUES (?,?,?,?);';
-            const valuesItem = [rowsPedido.insertId, pIdProduto, pQuantidadeItem, pValorItem];
-            const [rowsItem] = await connection.query(sqlItem, valuesItem);
 
             connection.commit();
 
             return { rowsPedido, rowsItem }
-        } catch (error) {
-            connection.rollback();
-            throw error;
-        }
-    },
-    insertItem: async (pIdPedido, pIdProduto, pQuantidadeItem, pValorItem) => {
-        const connection = await pool.getConnection();
-        try {
-            await connection.beginTransaction();
-            const sqlItem = 'INSERT INTO itens_pedidos (id_pedido_fk, id_produto_fk, quantidade, valor_item) VALUES (?,?,?,?);';
-            const valuesItem = [pIdPedido, pIdProduto, pQuantidadeItem, pValorItem]
-            const [rowsItem] = await connection.query(sqlItem, valuesItem);
-
-            const sqlPedido = 'UPDATE pedidos SET valor_total = valor_total +(?*?) WHERE id_pedido = ?';
-            const valuesPedido = [pQuantidadeItem, pValorItem, pIdPedido];
-            const [rowsPedido] = await connection.query(sqlPedido, valuesPedido);
-
-            connection.commit();
-            return { rowsItem, rowsPedido };
         } catch (error) {
             connection.rollback();
             throw error;
